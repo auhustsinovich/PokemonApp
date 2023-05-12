ca45be281fd42b4d04c8c275c667ed26fdb7868f
Daniel Augustynowicz <d.augustynowicz@andersenlab.com>
2023-05-12 09:54:32 +0200

    Initial Commit

---
+++ PokemonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by Daniil Auhustsinovich on 12.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
