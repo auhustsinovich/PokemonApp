3718cc3df90181b687d3af0e111c8bdb47782b61
Daniel Augustynowicz <d.augustynowicz@andersenlab.com>
2023-05-12 12:54:49 +0200

    Add models and commonDefaultValues file

---
+++ PokemonApp/Presentation/Models/PokemonList.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+/// A struct representing a list of Pokemon.
+struct PokemonList: Decodable {
+    let results: [Pokemon]
+}
+
+struct Pokemon: Decodable {
+    let name: String
+    let url: String
+
+    var id: Int? {
+        return Int(url.split(separator: "/").last?.description ?? "0")
+    }
+
+    var imageURL: URL? {
+        return URL(string: "\(CommonDefaultValues.imageUrl)\(id ?? 0).png")
+    }
+}
