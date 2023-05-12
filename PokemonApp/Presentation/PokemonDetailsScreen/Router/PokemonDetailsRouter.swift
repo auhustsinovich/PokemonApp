ce8ca75367f3a27fa3c7b200583287010a59ec0c
Daniel Augustynowicz <d.augustynowicz@andersenlab.com>
2023-05-12 15:52:46 +0200

    Complete business logic for Details Screen

--- PokemonApp/Presentation/PokemonDetailsScreen/Router/PokemonDetailsRouter.swift
+++ PokemonApp/Presentation/PokemonDetailsScreen/Router/PokemonDetailsRouter.swift
@@ -1,2 +1,25 @@
 import UIKit
 
+private let pokemonDetailsStoryboardName = "PokemonDetailsStoryboard"
+
+extension Router {
+    private func initDetailsScreen(pokemon: PokemonDetailViewModel) -> PokemonDetailsViewController {
+        let storyboard = UIStoryboard(name: pokemonDetailsStoryboardName, bundle: nil)
+
+        guard let view = storyboard.instantiateInitialViewController() as? PokemonDetailsViewController else {
+            return PokemonDetailsViewController()
+        }
+
+        let router = Router(view: view)
+        let presenter = PokemonDetailsPresenter(view: view, router: router, pokemon: pokemon)
+        view.presenter = presenter
+        return view
+    }
+
+    func goToDetailsScreen(pokemon: PokemonDetailViewModel) {
+        if let view = view as? PokemonListViewController {
+            let viewController = initDetailsScreen(pokemon: pokemon)
+            view.navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+}
